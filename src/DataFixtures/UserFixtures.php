02b86d327ce2094c85727bf080eab5016cfc1a26
koromerzhin <letoullec.martial@gmail.com>
2019-03-08 15:46:47 +0100

    Chore: Add user Authentification

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use App\Entity\User;
+
+class UserFixtures extends Fixture
+{
+    /**
+     * password Encoder
+     *
+     * @var UserPasswordEncoderInterface
+     */
+    private $passwordEncoder;
+
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $this->passwordEncoder = $passwordEncoder;
+    }
+
+    public function load(ObjectManager $manager)
+    {
+        $user = new User();
+        $user->setUsername('admin');
+        $encodePassword = $this->passwordEncoder->encodePassword(
+            $user,
+            "password"
+        );
+        $user->setPassword($encodePassword);
+        $manager->persist($user);
+
+        $manager->flush();
+    }
+}
