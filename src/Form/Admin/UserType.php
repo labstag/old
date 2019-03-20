4c99a5d056a2e83ba87f275786990e0a5ea3c248
koromerzhin <letoullec.martial@gmail.com>
2019-03-21 00:34:53 +0100

    :wrench: Corvée (#15): Ajout d'une table de configuration

--- src/Form/Admin/UserType.php
+++ src/Form/Admin/UserType.php
@@ -12,7 +12,6 @@ class UserType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder->add('username');
-        $builder->add('roles');
         $builder->add('password');
     }
 
