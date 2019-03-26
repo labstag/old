9e7f2e3a2f83c10d03ba8671471c4f4c5d05dfcb
koromerzhin <letoullec.martial@gmail.com>
2019-03-27 00:52:32 +0100

    :wrench: Corvée: Remplacement Deprecated functions

--- src/Controller/EasyAdminController.php
+++ src/Controller/EasyAdminController.php
@@ -2,23 +2,25 @@
 
 namespace App\Controller;
 
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use EasyCorp\Bundle\EasyAdminBundle\Controller\AdminController;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AdminControllerTrait;
 use Faker\Factory;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 /**
  * @Route("/backend")
  */
-class EasyAdminController extends AdminController
+class EasyAdminController extends AbstractController
 {
+    use AdminControllerTrait;
+
     /**
      * @Route("/dashboard", name="easyadmin_dashboard")
-     *
-     * @return void
      */
     public function dashboard()
     {
         $faker = Factory::create('fr_FR');
+
         return $this->render(
             'easyadmin/dashboard.html.twig'
         );
