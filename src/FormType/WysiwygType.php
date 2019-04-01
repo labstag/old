ef4d73abc9a690eb9d1f262636786480c5ec94cc
koromerzhin <letoullec.martial@gmail.com>
2019-04-01 17:40:22 +0200

    :wrench: Corvée: Add Wysiwg Type Form

---
+++ src/FormType/WysiwygType.php
@@ -0,0 +1,44 @@
+<?php
+namespace App\FormType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Form\FormView;
+
+class WysiwygType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     *
+     * @param FormView      $view    view
+     * @param FormInterface $form    form
+     * @param array         $options options
+     *
+     * @return void
+     */
+    public function buildView(FormView $view, FormInterface $form, array $options): void
+    {
+        $attr = $options['attr'];
+        if (!isset($attr['class'])) {
+            $attr['class'] = '';
+        }
+
+        $attr['class'] = trim($attr['class'] . ' wysiwyg');
+        if (!isset($attr['rows'])) {
+            $attr['rows'] = 10;
+        }
+
+        $view->vars['attr'] = $attr;
+        unset($form);
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @return string
+     */
+    public function getParent(): string
+    {
+        return TextareaType::class;
+    }
+}
