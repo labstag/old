79da8eedf56dff0a4c1e0f038d9865e348b2cdff
koromerzhin <letoullec.martial@gmail.com>
2019-04-02 17:41:36 +0200

    :zapr: Performance: Test for debugger

--- webpack.config.js
+++ webpack.config.js
@@ -1,115 +1,130 @@
 const path = require('path')
+
 const CleanWebpackPlugin = require('clean-webpack-plugin')
+
 const ManifestPlugin = require('webpack-assets-manifest')
+
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
+
 const webpack = require('webpack')
+
 const dev = (process.env.NODE_ENV == 'dev')
-let config = {
+
+const config = {
     'mode': dev ? 'development' : 'production',
     'entry': {
         'site': [
             './assets/site.js',
-            './assets/site.scss'
-        ]
+            './assets/site.scss',
+        ],
     },
-    resolve: {
-        alias: {
-            node_modules: path.resolve(__dirname, 'node_modules')
+    "devtool": dev ? 'eval-source-map' : null,
+    "resolve": {
+        "alias": {
+            "node_modules": path.resolve(__dirname, 'node_modules'),
         },
-        modules: [
+        "modules": [
             'node_modules',
-            path.resolve(__dirname, 'node_modules')
-        ]
+            path.resolve(__dirname, 'node_modules'),
+        ],
     },
     'devtool': dev ? 'cheap-module-eval-source-map' : '',
     'module': {
-        'rules': [{
-                'enforce': 'pre',
-                'test': /\.js$/,
-                'exclude': /node_modules/,
-                'use': ['babel-loader', 'eslint-loader']
-            },
-            {
-                'test': /\.js$/,
-                'exclude': /node_modules/,
-                'use': [{
-                    'loader': 'babel-loader',
-                    'options': {
-                        'presets': [
-                            ['@babel/preset-env']
-                        ],
-                        'plugins': [
-                            'syntax-dynamic-import'
-                        ]
-                    }
-                }]
-            },
-            {
-                'test': /\.(sa|sc|c)ss$/,
-                'use': [
-                    MiniCssExtractPlugin.loader,
-                    'css-loader',
-                    'sass-loader'
-                ]
-            },
-            {
-                'test': /\.(woff2|woff|eot|ttf|otf)(\?.*)?$/,
-                'loader': 'file-loader'
-            },
-            {
-                'test': /\.(png|jpe?g|gif|svg)$/,
-                'use': [{
-                    'loader': 'url-loader',
-                    'options': {
-                        'name': '[name].[hash:7].[ext]',
-                        'limit': 8192
-                    }
-                }]
+        'rules': [
+{
+            'enforce': 'pre',
+            'test': /\.js$/,
+            'exclude': /node_modules/,
+            'use': [
+'babel-loader', 'eslint-loader'
+,],
+        },
+        {
+            'test': /\.js$/,
+            'exclude': /node_modules/,
+            'use': [
+{
+                'loader': 'babel-loader',
+                'options': {
+                    'presets': [['@babel/preset-env',],],
+                    'plugins': ['syntax-dynamic-import',],
+                },
             }
-        ]
+,],
+        },
+        {
+            'test': /\.(sa|sc|c)ss$/,
+            'use': [
+                MiniCssExtractPlugin.loader,
+                'css-loader',
+                'sass-loader',
+            ],
+        },
+        {
+            'test': /\.(woff2|woff|eot|ttf|otf)(\?.*)?$/,
+            'loader': 'file-loader',
+        },
+        {
+            'test': /\.(png|jpe?g|gif|svg)$/,
+            'use': [
+{
+                'loader': 'url-loader',
+                'options': {
+                    'name': '[name].[hash:7].[ext]',
+                    'limit': 8192,
+                },
+            }
+,],
+        },
+        ],
     },
     'output': {
         'crossOriginLoading': 'anonymous',
         'path': path.resolve(__dirname, 'public/assets'),
         'filename': '[name].[chunkhash:8].js',
-        'publicPath': ''
+        'publicPath': '',
     },
     'plugins': [
         new MiniCssExtractPlugin({
             'filename': '[name].[contenthash:8].css',
-            'chunkFilename': '[id].[hash].css'
+            'chunkFilename': '[id].[hash].css',
         }),
         new ManifestPlugin({
-            output: 'manifest.json',
-            integrityHashes: ['sha256'],
-            integrity: true
+            "output": 'manifest.json',
+            "integrityHashes": ['sha256',],
+            "integrity": true,
         }),
-        // new CleanWebpackPlugin(
-        //     ['assets'], {
-        //         'root'   : path.resolve('./public/'),
-        //         'verbose': true,
-        //         'dry'    : false
-        //     }
-        // ),
+        /*
+         * new CleanWebpackPlugin(
+         *     ['assets'], {
+         *         'root'   : path.resolve('./public/'),
+         *         'verbose': true,
+         *         'dry'    : false
+         *     }
+         * ),
+         */
         new webpack.ProvidePlugin({
-            $: 'jquery',
-            jQuery: 'jquery',
+            "$": 'jquery',
+            "jQuery": 'jquery',
             'window.jQuery': 'jquery',
             'window.$': 'jquery',
-        })
-    ]
+        }),
+    ],
 }
 
 if (!dev) {
     config.plugins.push(
-        // new UglifyJSPlugin({
-        //     comments     : false,
-        //     uglifyOptions: {
-        //         compress: true,
-        //         warnings: false,
-        //     },
-        // })
+        /*
+         * new UglifyJSPlugin({
+         *     comments     : false,
+         *     uglifyOptions: {
+         *         compress: true,
+         *         warnings: false,
+         *     },
+         * })
+         */
     )
 }
 
