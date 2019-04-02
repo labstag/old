d0601de5b2744c1b2bd7643e5b8068095a294324
koromerzhin <letoullec.martial@gmail.com>
2019-04-02 14:31:25 +0200

    :sparkles: Fonctionnalité: Add rules to eslint

--- assets/site.js
+++ assets/site.js
@@ -1,1 +1,19 @@
 import 'bootstrap';
+class Site {
+    constructor() {
+        console.log('test');
+    }
+
+    /**
+     * TODO: Test
+     */
+    launch() {
+        console.warn('coucou');
+        console.error('test');
+        console.debug('test');
+        debugger;
+        alert('coucou');
+    }
+}
+let site = new Site();
+site.launch();
