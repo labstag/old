17519f98ee56e4ea7b5906ec8f645218571719bc
koromerzhin <letoullec.martial@gmail.com>
2019-04-04 10:43:58 +0200

    :hammer: Refactor: Moins de code pour la connexion

--- src/Lib/ConnectAbstractControllerLib.php
+++ src/Lib/ConnectAbstractControllerLib.php
@@ -3,21 +3,46 @@
 namespace App\Lib;
 
 use KnpU\OAuth2ClientBundle\Client\ClientRegistry;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 use League\OAuth2\Client\Provider\Exception\IdentityProviderException;
 
 abstract class ConnectAbstractControllerLib extends AbstractControllerLib
 {
-    protected function connectCheck(
-        ClientRegistry $clientRegistry,
-        string $clientName
+
+    /**
+     * @var ClientRegistry
+     */
+    private $clientRegistry;
+
+    public function __construct(
+        ContainerInterface $container,
+        ClientRegistry $clientRegistry
     )
+    {
+        $this->clientRegistry = $clientRegistry;
+        parent::__construct($container);
+    }
+
+    protected function connectRedirect(string $clientName)
+    {
+        $service  = $this->clientRegistry->getClient($clientName.'_main');
+        $redirect = $service->redirect(
+            [
+            'public_profile', 'email' // the scopes you want to access
+            ]
+        );
+
+        return $redirect;
+    }
+
+    protected function connectCheck(string $clientName)
     {
         // ** if you want to *authenticate* the user, then
         // leave this method blank and create a Guard authenticator
         // (read below)
 
         /** @var \KnpU\OAuth2ClientBundle\Client\Provider\GithubClient $client */
-        $client = $clientRegistry->getClient($clientName);
+        $client = $this->clientRegistry->getClient($clientName.'_main');
 
         try {
             // the exact class depends on which provider you're using
@@ -27,12 +52,14 @@ abstract class ConnectAbstractControllerLib extends AbstractControllerLib
             // do something with all this new power!
 	        // e.g. $name = $user->getFirstName();
             dump($user);
+            exit();
             return;
             // ...
         } catch (IdentityProviderException $e) {
             // something went wrong!
             // probably you should return the reason to the user
             dump($e->getMessage());
+            exit();
             return;
         }
     }
