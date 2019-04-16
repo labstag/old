130b1294e76474b15d85075d001811ace9727c13
koromerzhin <letoullec.martial@gmail.com>
2019-04-16 17:59:35 +0200

    :wrench: Corvée: Pour un futur travail sur datatables

--- assets/js/app.js
+++ assets/js/app.js
@@ -19,87 +19,94 @@ global.$ = $;
 global.Jquery = $;
 class Site {
 
-  /**
-   * TODO: Test
-   */
-  launch() {
-    this.setWysiwyg();
-    this.setDatatables();
-    const NUMBER = 1;
+    /**
+     * TODO: Test
+     */
+    launch() {
+        this.setWysiwyg();
+        this.setDatatables();
+        const NUMBER = 1;
 
-    let test = NUMBER;
+        let test = NUMBER;
 
-    console.warn('coucou');
-    console.error('test');
-    console.debug('test');
-    test += NUMBER;
+        console.warn('coucou');
+        console.error('test');
+        console.debug('test');
+        test += NUMBER;
 
-    return test;
-  }
+        return test;
+    }
 
-  setDatatables() {
-    let table = $('.dataTable').DataTable({
-      dom: 'Bfrtip',
-      buttons: [
-        'selected',
-        'selectedSingle',
-        'selectAll',
-        'selectNone',
-        'selectRows',
-        'selectColumns',
-        'selectCells',
-        'copy', 'csv', 'print'
-      ],
-      columnDefs: [{
-        orderable: false,
-        className: 'select-checkbox',
-        targets: 0
-      }],
-      select: {
-        style: 'multi'
-      }
-    });
-    global.table = table;
-  }
+    setDatatables() {
+        let $tables = $('.dataTable');
+        $($tables).each(
+            function () {
+                let $table = $(this);
+                $($table).DataTable({
+                    orderCellsTop: true,
+                    fixedHeader: true,
+                    dom: 'Bfrtip',
+                    buttons: [
+                        'selected',
+                        'selectedSingle',
+                        'selectAll',
+                        'selectNone',
+                        'selectRows',
+                        'selectColumns',
+                        'selectCells',
+                        'copy', 'csv', 'print'
+                    ],
+                    columnDefs: [{
+                        orderable: false,
+                        className: 'select-checkbox',
+                        targets: 0
+                    }],
+                    select: {
+                        style: 'multi'
+                    }
+                });
+            }
+        );
+    }
 
-  setWysiwyg() {
-    console.log('aa');
-    tinymce.init({
-      branding: false,
-      selector: '.wysiwyg',
-      height: 400,
-      image_title: true,
-      automatic_uploads: true,
-      file_picker_types: 'image',
-      images_upload_url: 'images.php',
-      file_picker_callback: function (cb, value, meta) {
-        let input = document.createElement('input');
-        input.setAttribute('type', 'file');
-        input.setAttribute('accept', 'image/*');
-        input.onchange = function () {
-          let file = this.files[0];
-          let reader = new FileReader();
-          reader.onload = function () {
-            let id = 'blobid' + (new Date()).getTime();
-            let blobCache = tinymce.activeEditor.editorUpload.blobCache;
-            let base64 = reader.result.split(',')[1];
-            let blobInfo = blobCache.create(id, file, base64);
-            blobCache.add(blobInfo);
-            cb(blobInfo.blobUri(), {
-              title: file.name
-            });
-          };
-          reader.readAsDataURL(file);
-        };
-        input.click();
-      },
-      relative_urls: false,
-      plugins: 'visualblocks print preview fullpage searchreplace autolink directionality visualblocks visualchars fullscreen image link media template codesample table charmap hr pagebreak nonbreaking anchor toc insertdatetime advlist lists wordcount imagetools textpattern help',
-      toolbar: 'formatselect | bold italic strikethrough forecolor backcolor | link image media | alignleft aligncenter alignright alignjustify  | numlist bullist outdent indent | removeformat | addcomment',
-      language: 'fr_FR',
-      'images_upload_handler'(blobInfo, success, failure) {},
-    });
-  }
+    setWysiwyg() {
+        console.log('aa');
+        tinymce.init({
+            branding: false,
+            selector: '.wysiwyg',
+            height: 400,
+            image_title: true,
+            automatic_uploads: true,
+            file_picker_types: 'image',
+            images_upload_url: 'images.php',
+            file_picker_callback: function (cb, value, meta) {
+                let input = document.createElement('input');
+                input.setAttribute('type', 'file');
+                input.setAttribute('accept', 'image/*');
+                input.onchange = function () {
+                    let file = this.files[0];
+                    let reader = new FileReader();
+                    reader.onload = function () {
+                        let id = 'blobid' + (new Date()).getTime();
+                        let blobCache = tinymce.activeEditor.editorUpload.blobCache;
+                        let base64 = reader.result.split(',')[1];
+                        let blobInfo = blobCache.create(id, file, base64);
+                        blobCache.add(blobInfo);
+                        cb(blobInfo.blobUri(), {
+                            title: file.name
+                        });
+                    };
+                    reader.readAsDataURL(file);
+                };
+                input.click();
+            },
+            relative_urls: false,
+            plugins: 'visualblocks print preview fullpage searchreplace autolink directionality visualblocks visualchars fullscreen image link media template codesample table charmap hr pagebreak nonbreaking anchor toc insertdatetime advlist lists wordcount imagetools textpattern help',
+            toolbar: 'formatselect | bold italic strikethrough forecolor backcolor | link image media | alignleft aligncenter alignright alignjustify  | numlist bullist outdent indent | removeformat | addcomment',
+            language: 'fr_FR',
+            'images_upload_handler'(blobInfo, success, failure) {},
+        });
+    }
 
 }
 const site = new Site();
