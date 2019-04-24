5099f523a4aefb9a8dfa0c18234a12ca21ef3c68
koromerzhin <letoullec.martial@gmail.com>
2019-04-24 10:29:54 +0200

    :hammer: Refactor: Séparation des modules Js

---
+++ src/Controller/Admin/WorkflowAdmin.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Labstag\Controller\Admin;
+
+use Labstag\Lib\AdminAbstractControllerLib;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/admin/workflow")
+ */
+class WorkflowAdmin extends AdminAbstractControllerLib
+{
+    /**
+     * @Route("/", name="adminworkflow_index")
+     */
+    public function index(Request $request): Response
+    {
+
+        return $this->twig(
+            'admin/workflow.html.twig',
+            [
+                'title' => 'workflow'
+            ]
+        );
+    }
+}
