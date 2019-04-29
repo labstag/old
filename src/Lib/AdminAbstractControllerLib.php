5956870c6b5991e494a07a75fe56570bc140c650
koromerzhin <letoullec.martial@gmail.com>
2019-04-29 17:40:53 +0200

    :wrench: Corvée: Add plugins to datatables

--- src/Lib/AdminAbstractControllerLib.php
+++ src/Lib/AdminAbstractControllerLib.php
@@ -2,13 +2,14 @@
 
 namespace Labstag\Lib;
 
-use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpKernel\Exception\HttpException;
 
 abstract class AdminAbstractControllerLib extends AbstractControllerLib
 {
+    
     protected function crudListAction(ServiceEntityRepositoryLib $repository)
     {
         $entities = $repository->findAll();
