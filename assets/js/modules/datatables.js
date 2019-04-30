f02107715d2bb11754c1a1c84ab368033a02bb65
koromerzhin <letoullec.martial@gmail.com>
2019-04-30 17:18:32 +0200

    :wrench: Corvée: Change format date

--- assets/js/modules/datatables.js
+++ assets/js/modules/datatables.js
@@ -1,13 +1,16 @@
 import 'bootstrap-table';
 import 'bootstrap-table/dist/bootstrap-table-locale-all';
+var moment = require('moment');
+
 export class datatables {
     constructor() {
         this.execute();
+        moment.locale('fr');
     }
     execute() {
         window.dateFormatter = this.dateFormatter;
     }
     dateFormatter(value, row) {
-        return value;
+        return moment(value).format('MMMM Do YYYY, h:mm:ss a');
     }
 }
