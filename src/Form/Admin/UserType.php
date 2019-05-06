24affb66d9db065804b49ee9c93ebc7187c3097f
koromerzhin <letoullec.martial@gmail.com>
2019-05-07 00:55:50 +0200

    :wrench: Corvée: Support Upload file

--- src/Form/Admin/UserType.php
+++ src/Form/Admin/UserType.php
@@ -18,7 +18,7 @@ class UserType extends AbstractType
         $builder->add('enable');
         $builder->add('apiKey');
         $builder->add('enable');
-        $builder->add('avatar');
+        $builder->add('imageFile');
         $builder->add('submit', SubmitType::class);
         unset($options);
     }
