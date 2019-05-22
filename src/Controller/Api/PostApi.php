d0d53aa607bd872d5ce5e13d02695429b0536b6b
koromerzhin <letoullec.martial@gmail.com>
2019-05-22 14:15:33 +0200

    :art: art: PHPCSFIXER + PHPCS

--- src/Controller/Api/PostApi.php
+++ src/Controller/Api/PostApi.php
@@ -3,9 +3,8 @@
 namespace Labstag\Controller\Api;
 
 use Labstag\Lib\ApiControllerLib;
-use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\Routing\Annotation\Route;
 use Labstag\Repository\PostRepository;
+use Symfony\Component\Routing\Annotation\Route;
 
 class PostApi extends ApiControllerLib
 {
