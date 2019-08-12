db81877445c4aeb8c0e24ab329b1510e1b54519e
koromerzhin <letoullec.martial@gmail.com>
2019-08-12 10:16:15 +0200

    :bug: bug: Remplace form input file by vichupload

--- src/Form/Admin/UserType.php
+++ src/Form/Admin/UserType.php
@@ -7,6 +7,7 @@ use Labstag\Lib\AbstractTypeLib;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Vich\UploaderBundle\Form\Type\VichImageType;
 
 class UserType extends AbstractTypeLib
 {
@@ -18,7 +19,7 @@ class UserType extends AbstractTypeLib
         $builder->add('enable');
         $builder->add('apiKey');
         $builder->add('enable');
-        $builder->add('imageFile');
+        $builder->add('imageFile', VichImageType::class);
         $builder->add('submit', SubmitType::class);
         unset($options);
     }
