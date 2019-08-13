c07c18b45d38f2cf10a8e536a34db84c2cc4eb02
koromerzhin <letoullec.martial@gmail.com>
2019-08-13 21:36:48 +0200

    :bug: bug: Possibilité d'ajouter des tags directement par le formulaire

--- src/Controller/Admin/TemporaryAdmin.php
+++ src/Controller/Admin/TemporaryAdmin.php
@@ -12,6 +12,7 @@ use Labstag\Repository\TagsRepository;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Labstag\Repository\CategoryRepository;
 
 /**
  * @Route("/admin/temporary")
@@ -23,13 +24,37 @@ class TemporaryAdmin extends AdminControllerLib
      *
      * @return Response
      */
-    public function tags($type): JsonResponse
+    public function tags(TagsRepository $repository, string $type): JsonResponse
     {
-        $tags = new Tags();
-        $tags->setType($type);
+        $tabs = [
+            'data' => []
+        ];
         $post = $this->request->request->all();
-        $data = [];
-        return new JsonResponse($post);
+        if (isset($post['req'])) {
+            $search = [
+                'type' => $type,
+                'name' => $post['req']
+            ];
+            $tag    = $repository->findOneBy($search);
+            if (!$tag) {
+                $tags = new Tags();
+                $tags->setType($type);
+                $tags->setName($post['req']);
+                $tags->setTemporary(true);
+                $this->persistAndFlush($tags);
+            }
+        }
+
+        $data    = $repository->findTagsByType($type);
+        $query   = $data->getQuery();
+        $results = $query->getResult();
+        foreach ($results as $row) {
+            $tabs['data'][] = [
+                'value' => $row->getId(),
+                'name'  => $row->getName(),
+            ];
+        };
+        return new JsonResponse($tabs);
     }
 
     /**
@@ -37,8 +62,9 @@ class TemporaryAdmin extends AdminControllerLib
      *
      * @return Response
      */
-    public function category(): JsonResponse
+    public function category(CategoryRepository $repository): JsonResponse
     {
+        // $repository->find
         $data = [];
         return new JsonResponse($data);
     }
