dcd3b8ba2f36e8342db4fc7af4d8cc1f2412ee83
koromerzhin <letoullec.martial@gmail.com>
2019-09-02 13:52:48 +0200

    :sparkles: sparkles: Add Email / Telephone

---
+++ src/Entity/Email.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace Labstag\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ApiResource()
+ * @ORM\Entity(repositoryClass="Labstag\Repository\EmailRepository")
+ */
+class Email
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="UUID")
+     * @ORM\Column(type="guid", unique=true)
+     */
+    private $id;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="Labstag\Entity\User", inversedBy="emails")
+     */
+    private $refuser;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $adresse;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $principal;
+
+    public function getId(): ?string
+    {
+        return $this->id;
+    }
+
+    public function getRefuser(): ?User
+    {
+        return $this->refuser;
+    }
+
+    public function setRefuser(?User $refuser): self
+    {
+        $this->refuser = $refuser;
+
+        return $this;
+    }
+
+    public function getAdresse(): ?string
+    {
+        return $this->adresse;
+    }
+
+    public function setAdresse(string $adresse): self
+    {
+        $this->adresse = $adresse;
+
+        return $this;
+    }
+
+    public function getPrincipal(): ?bool
+    {
+        return $this->principal;
+    }
+
+    public function setPrincipal(bool $principal): self
+    {
+        $this->principal = $principal;
+
+        return $this;
+    }
+}
---
+++ src/Entity/Telephone.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace Labstag\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ApiResource()
+ * @ORM\Entity(repositoryClass="Labstag\Repository\TelephoneRepository")
+ */
+class Telephone
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="UUID")
+     * @ORM\Column(type="guid", unique=true)
+     */
+    private $id;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="Labstag\Entity\User", inversedBy="telephones")
+     */
+    private $refuser;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $numero;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $type;
+
+    public function getId(): ?string
+    {
+        return $this->id;
+    }
+
+    public function getRefuser(): ?User
+    {
+        return $this->refuser;
+    }
+
+    public function setRefuser(?User $refuser): self
+    {
+        $this->refuser = $refuser;
+
+        return $this;
+    }
+
+    public function getNumero(): ?string
+    {
+        return $this->numero;
+    }
+
+    public function setNumero(string $numero): self
+    {
+        $this->numero = $numero;
+
+        return $this;
+    }
+
+    public function getType(): ?string
+    {
+        return $this->type;
+    }
+
+    public function setType(string $type): self
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+}
