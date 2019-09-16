50495dd4833605b8166cad2c599280b9cc1644dc
Restyled.io <commits@restyled.io>
2019-09-16 07:33:44 +0000

    Restyled by prettier

--- assets/js/modules/xhr.js
+++ assets/js/modules/xhr.js
@@ -1,21 +1,21 @@
 export class xhr {
-    constructor() {
-
-    }
-    delete(url, data) {
-        window.fetch(
-            url, {
-                'method': 'DELETE',
-                'body'  : JSON.stringify(data)
-            }
-        ).then((response) => {
-            return response.text();
-        } ).then((text) => {
-            return JSON.parse(text);
-        } ).then((json) => {
-            if (json.redirect != undefined) {
-                window.location.replace(json.redirect);
-            }
-        } );
-    }
+  constructor() {}
+  delete(url, data) {
+    window
+      .fetch(url, {
+        method: "DELETE",
+        body: JSON.stringify(data)
+      })
+      .then(response => {
+        return response.text();
+      })
+      .then(text => {
+        return JSON.parse(text);
+      })
+      .then(json => {
+        if (json.redirect != undefined) {
+          window.location.replace(json.redirect);
+        }
+      });
+  }
 }
