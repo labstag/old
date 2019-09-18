10bdd1e705d0bfe53a4ec2244f04752d03597a3d
koromerzhin <letoullec.martial@gmail.com>
2019-09-18 13:37:29 +0200

    :construction: construction: test repository

---
+++ tests/Repository/PhoneTest.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Labstag\Tests\Repository;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+class PhoneTest extends ServiceEntityRepository
+{
+}
---
+++ tests/Repository/TagsTest.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Labstag\Tests\Repository;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+class TagsTest extends WebTestCase
+{
+}
