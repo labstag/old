ad6ffee6a3859f912823064ab134d8c8625e4fd4
Martial Le TOULLEC <mletoullec@itlink.fr>
2019-12-10 14:10:03 +0100

    :alembic: alembic: Remplace Jquery By VanillaJs

--- assets/js/modules/datatables.js
+++ assets/js/modules/datatables.js
@@ -1,24 +1,26 @@
-import "whatwg-fetch";
-import "jquery-resizable-columns";
-import "bootstrap-table";
-import "tableexport.jquery.plugin/tableExport.min";
-import "bootstrap-table/dist/bootstrap-table-locale-all";
-import "bootstrap-table/dist/extensions/resizable/bootstrap-table-resizable";
-import "bootstrap-table/dist/extensions/toolbar/bootstrap-table-toolbar";
-import "bootstrap-table/dist/extensions/export/bootstrap-table-export";
-import "bootstrap-table/dist/extensions/page-jump-to/bootstrap-table-page-jump-to.min";
-import "bootstrap-table/dist/extensions/multiple-sort/bootstrap-table-multiple-sort";
-import "bootstrap-table/dist/extensions/mobile/bootstrap-table-mobile";
-import "bootstrap-table/dist/extensions/cookie/bootstrap-table-cookie";
-import "@fancyapps/fancybox";
-let moment = require("moment");
+import 'whatwg-fetch';
+import 'jquery-resizable-columns';
+import 'bootstrap-table';
+import 'tableexport.jquery.plugin/tableExport.min';
+import 'bootstrap-table/dist/bootstrap-table-locale-all';
+import 'bootstrap-table/dist/extensions/resizable/bootstrap-table-resizable';
+import 'bootstrap-table/dist/extensions/toolbar/bootstrap-table-toolbar';
+import 'bootstrap-table/dist/extensions/export/bootstrap-table-export';
+import 'bootstrap-table/dist/extensions/page-jump-to/bootstrap-table-page-jump-to.min';
+import 'bootstrap-table/dist/extensions/multiple-sort/bootstrap-table-multiple-sort';
+import 'bootstrap-table/dist/extensions/mobile/bootstrap-table-mobile';
+import 'bootstrap-table/dist/extensions/cookie/bootstrap-table-cookie';
+import '@fancyapps/fancybox';
+let moment = require('moment');
 
 export class datatables {
   constructor() {
     this.execute();
-    moment.locale($("body").attr("data-momentlang"));
+    let body = document.querySelector('body');
+
+    moment.locale(body.getAttribute('data-momentlang'));
   }
-  
+
   setWindows() {
     window.dateFormatter      = this.dateFormatter;
     window.imageFormatter     = this.imageFormatter;
@@ -34,26 +36,26 @@ export class datatables {
   execute() {
     this.setWindows();
     window.ajaxOptions = {
-      headers: {
-        Accept: "application/ld+json"
+      "headers": {
+        "Accept": 'application/ld+json'
       },
-      dataFilter: function(data) {
+      "dataFilter": function (data) {
         var json = JSON.parse(data);
         var data = {
-          count: json["hydra:totalItems"],
-          totalNotFiltered: json["hydra:totalItems"],
-          rows: json["hydra:member"],
-          total: json["hydra:member"].length
+          "count"           : json['hydra:totalItems'],
+          "totalNotFiltered": json['hydra:totalItems'],
+          "rows"            : json['hydra:member'],
+          "total"           : json['hydra:member'].length
         };
 
         return JSON.stringify(data);
       }
     };
 
-    $("#CrudList").on("post-header.bs.table", this.postheader.bind(this));
+    $('#CrudList').on('post-header.bs.table', this.postheader.bind(this));
     $(document).on(
-      "change",
-      ".custom-control-input",
+      'change',
+      '.custom-control-input',
       this.changeEnable.bind(this)
     );
   }
@@ -70,26 +72,26 @@ export class datatables {
   }
 
   launchData(url) {
-    window
-      .fetch(url, {})
-      .then(response => {
+    window.
+      fetch(url, {} ).
+      then((response) => {
         return response.text();
-      })
-      .then(text => {
+      } ).
+      then((text) => {
         return JSON.parse(text);
-      })
-      .then(json => {
+      } ).
+      then((json) => {
         this.dataTreat(json);
-      });
+      } );
   }
 
   dataTreat(json) {
-    const id   = json["@id"];
-    const type = json["@type"];
+    const id   = json['@id'];
+    const type = json['@type'];
 
     if (json.name != undefined) {
       this.changeDiv(id, json.name);
-    } else if (type == "User") {
+    } else if (type == 'User') {
       this.changeDiv(id, json.username);
     } else {
       console.log(type);
@@ -97,49 +99,49 @@ export class datatables {
   }
 
   changeDiv(id, value) {
-    document.querySelectorAll(".DataSpan").forEach(element => {
-      if (element.getAttribute("data-id") == id) {
+    document.querySelectorAll('.DataSpan').forEach((element) => {
+      if (element.getAttribute('data-id') == id) {
         element.innerHTML = value;
       }
-    });
+    } );
   }
 
   dataFormatter(value, row) {
     if (value != null) {
       window.urlData[value] = 1;
     }
-    let span = document.createElement("span");
+    let span = document.createElement('span');
 
-    span.setAttribute("data-id", value);
-    span.setAttribute("class", "DataSpan");
-    span.innerHTML = "";
+    span.setAttribute('data-id', value);
+    span.setAttribute('class', 'DataSpan');
+    span.innerHTML = '';
 
     return span.outerHTML;
   }
 
   changeEnable(event) {
     const element = $(event.currentTarget);
-    const enable  = $(element).attr("data-enable");
-    const state   = $(element).is(":checked");
-    const table   = $(element).closest("table");
-    const idTable = $(table).attr("id");
-    const url     = $("#" + idTable).attr("data-enableurl-" + enable);
+    const enable  = $(element).attr('data-enable');
+    const state   = $(element).is(':checked');
+    const table   = $(element).closest('table');
+    const idTable = $(table).attr('id');
+    const url     = $('#' + idTable).attr('data-enableurl-' + enable);
 
     window.fetch(url, {
-      method: "POST",
-      body: JSON.stringify({
-        state: state,
-        id: $(element).attr("data-id")
-      })
-    });
+      "method": 'POST',
+      "body"  : JSON.stringify( {
+        "state": state,
+        "id"   : $(element).attr('data-id')
+      } )
+    } );
   }
 
   end(value, row) {
-    return this.switch(value, row, "end");
+    return this.switch(value, row, 'end');
   }
 
   enable(value, row) {
-    return this.switch(value, row, "enable");
+    return this.switch(value, row, 'enable');
   }
 
   uniqid() {
@@ -147,15 +149,15 @@ export class datatables {
       new Date().getTime() + Math.floor(Math.random() * 10000 + 1)
     ).toString(16);
   }
-  
+
   setAttribute(input, data) {
     for (let [key, value] of Object.entries(array1)) {
-      input.setAttribute(key, value); 
+      input.setAttribute(key, value);
     }
   }
-  
+
   setInput(uniqId) {
-    const input = document.createElement("input");
+    const input = document.createElement('input');
     let data    = {
       'type'       : 'checkbox',
       'class'      : 'custom-control-input',
@@ -163,33 +165,36 @@ export class datatables {
       'id'         : `customSwitch${uniqId}`,
       'data-id'    : row.id
     };
+
     if (value == true) {
-      data['checked'] = 'checked';
+      data.checked = 'checked';
     }
-    
+
     this.setAttribute(input, data);
-    
+
     return input;
   }
-  
+
   setLabel(uniqId) {
-    const label = document.createElement("label");
+    const label = document.createElement('label');
     const data  = {
       'class': 'custom-control-label',
       'for'  : `customSwitch${uniqId}`
     };
+
     this.setAttribute(label, data);
-    
+
     return label;
   }
 
-  switch(value, row, id) {
-    const div    = document.createElement("div");
+  switch (value, row, id) {
+    const div    = document.createElement('div');
     const uniqId = this.uniqid();
 
-    div.setAttribute("class", "custom-control custom-switch");
+    div.setAttribute('class', 'custom-control custom-switch');
     const input = this.setInput(uniqId);
     const label = this.setLabel(uniqId);
+
     div.append(input);
     div.append(label);
 
@@ -201,21 +206,21 @@ export class datatables {
   }
 
   operations(value, row) {
-    const div    = document.querySelector(".OperationCrud");
-    const links  = div.querySelectorAll("a");
-    let html     = "";
+    const div    = document.querySelector('.OperationCrud');
+    const links  = div.querySelectorAll('a');
+    let html     = '';
     let idEntity = row.id;
 
     if (value != undefined) {
       idEntity = value;
     }
 
-    links.forEach(element => {
+    links.forEach((element) => {
       let a = element.outerHTML;
 
-      a = a.replace("code", idEntity);
+      a    = a.replace('code', idEntity);
       html = html + a;
-    });
+    } );
 
     return html;
   }
@@ -231,12 +236,12 @@ export class datatables {
     }
 
     delete params.offset;
-    if (params.search == "") {
+    if (params.search == '') {
       delete params.search;
     }
 
     if (params.sort != undefined) {
-      let val = "order[" + params.sort + "]";
+      let val  = 'order[' + params.sort + ']';
       let sort = params.order;
 
       delete params.order;
@@ -250,25 +255,26 @@ export class datatables {
   }
 
   dateFormatter(value, row) {
-    return moment(value).format("MMMM Do YYYY, H:mm:ss");
+    return moment(value).format('MMMM Do YYYY, H:mm:ss');
   }
 
   imageFormatter(value, row) {
     if (value != null) {
-      const link = document.createElement("a");
-      const img  = document.createElement("img");
-      const url  = $("#CrudList").attr("data-files");
+      const link = document.createElement('a');
+      const img  = document.createElement('img');
+      const url  = $('#CrudList').attr('data-files');
       const data = {
         'data-fancybox': true,
         'href'         : `${url}${value}`,
         'src'          : `${url}${value}`,
         'class'        : 'img-thumbnail'
       };
+
       this.setAttribute(link, data);
       link.appendChild(img);
       return link.outerHTML;
     }
 
-    return "";
+    return '';
   }
 }
