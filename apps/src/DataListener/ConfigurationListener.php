a2a17b0b9cfcd6e3a25a9cf748f10a35246a599f
koromerzhin <letoullec.martial@gmail.com>
2020-02-24 23:04:18 +0100

    :ambulance: ambulance: Show message if folder is not writeable

--- apps/src/DataListener/ConfigurationListener.php
+++ apps/src/DataListener/ConfigurationListener.php
@@ -4,11 +4,34 @@ namespace Labstag\DataListener;
 
 use Doctrine\ORM\Event\LifecycleEventArgs;
 use Doctrine\ORM\Events;
+use Exception;
 use Labstag\Entity\Configuration;
+use Psr\Log\LoggerInterface;
 use Labstag\Lib\EventSubscriberLib;
+use Symfony\Component\HttpFoundation\Session\Session;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 
 class ConfigurationListener extends EventSubscriberLib
 {
+
+    /**
+     * @var LoggerInterface
+     */
+    private $logger;
+
+    /**
+     * @var Session|SessionInterface
+     */
+    private $session;
+
+
+    public function __construct(LoggerInterface $logger, SessionInterface $session)
+    {
+        $this->logger = $logger;
+        /** @var Session $session */
+        $this->session = $session;
+    }
+
     /**
      * Sur quoi écouter.
      */
@@ -44,8 +67,15 @@ class ConfigurationListener extends EventSubscriberLib
     {
         $name  = $entity->getName();
         $value = $entity->getValue();
-        if ('robotstxt' == $name) {
-            file_put_contents('robots.txt', $value);
+        try {
+            if ('robotstxt' == $name) {
+                file_put_contents('robots.txt', $value);
+            }
+        } catch (Exception $exception) {
+            $this->logger->error($exception->getMessage());
+            /** @var Session $session */
+            $session = $this->session;
+            $session->getFlashBag()->add('danger', (string) $exception->getMessage());
         }
     }
 }
