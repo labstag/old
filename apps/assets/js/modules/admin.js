789cd74cf61b0e4c58cce8f3edf09dcacf56c3d3
Restyled.io <commits@restyled.io>
2020-02-26 13:39:24 +0000

    Restyled by clang-format

--- apps/assets/js/modules/admin.js
+++ apps/assets/js/modules/admin.js
@@ -1,93 +1,69 @@
-import {
-    workflow
-} from "./workflow";
-import {
-    xhr
-} from "./xhr";
+import {workflow} from "./workflow";
+import {xhr} from "./xhr";
 export class admin {
-    constructor() {
-        this.userList();
-        this.btndelete();
-        this.btnRestore();
-        this.sortable();
-        this.workflow = new workflow();
-        this.xhr = new xhr();
-    }
+  constructor() {
+    this.userList();
+    this.btndelete();
+    this.btnRestore();
+    this.sortable();
+    this.workflow = new workflow();
+    this.xhr = new xhr();
+  }
 
-    sortable() {
-        $("#sortable").sortable({
-            stop: this.positionChange
-        });
-    }
+  sortable() { $("#sortable").sortable({stop : this.positionChange}); }
 
-    positionChange(event, ui) {
-        $("#sortable")
-            .find("input")
-            .each(function (index) {
-                $(this).val(index);
-            });
-    }
-    btndelete() {
-        $(document).on(
-            "click",
-            ".OperationLinkDelete",
-            this.btndeleteOnClick.bind(this)
-        );
-    }
+  positionChange(event, ui) {
+    $("#sortable").find("input").each(function(index) { $(this).val(index); });
+  }
+  btndelete() {
+    $(document).on("click", ".OperationLinkDelete",
+                   this.btndeleteOnClick.bind(this));
+  }
 
-    btndeleteOnClick(event) {
-        event.preventDefault();
-        $(".BtnDeleteModalConfirm").attr(
-            "href",
-            $(event.currentTarget).attr("href")
-        );
-        $(".BtnDeleteModalConfirm").attr(
-            "data-id",
-            $(event.currentTarget).attr("data-id")
-        );
-        $(".BtnDeleteModalConfirm").off("click");
-        $(".BtnDeleteModalConfirm").on("click", this.confirmDelete.bind(this));
-        $("#deleteModal").modal();
-    }
+  btndeleteOnClick(event) {
+    event.preventDefault();
+    $(".BtnDeleteModalConfirm")
+        .attr("href", $(event.currentTarget).attr("href"));
+    $(".BtnDeleteModalConfirm")
+        .attr("data-id", $(event.currentTarget).attr("data-id"));
+    $(".BtnDeleteModalConfirm").off("click");
+    $(".BtnDeleteModalConfirm").on("click", this.confirmDelete.bind(this));
+    $("#deleteModal").modal();
+  }
 
-    btnRestore() {
-        $(document).on(
-            "click",
-            ".OperationLinkRestore",
-            this.btnRestoreOnclick.bind(this)
-        )
-    }
+  btnRestore() {
+    $(document).on("click", ".OperationLinkRestore",
+                   this.btnRestoreOnclick.bind(this))
+  }
 
-    btnRestoreOnclick(event) {
-        event.preventDefault();
-        $("#restoreModal").modal();
-    }
+  btnRestoreOnclick(event) {
+    event.preventDefault();
+    $("#restoreModal").modal();
+  }
 
-    confirmDelete(event) {
-        event.preventDefault();
-        let data = [];
-        let url = $(".BtnDeleteModalConfirm").attr("href");
-        let id = $(".BtnDeleteModalConfirm").attr("data-id");
+  confirmDelete(event) {
+    event.preventDefault();
+    let data = [];
+    let url = $(".BtnDeleteModalConfirm").attr("href");
+    let id = $(".BtnDeleteModalConfirm").attr("data-id");
 
-        data.push(id);
-        this.xhr.delete(url, data);
-    }
+    data.push(id);
+    this.xhr.delete(url, data);
+  }
 
-    userList() {
-        window.rolesFormatter = this.rolesFormatter;
-    }
+  userList() { window.rolesFormatter = this.rolesFormatter; }
 
-    rolesFormatter(roles, row) {
-        let ul = document.createElement("ul");
+  rolesFormatter(roles, row) {
+    let ul = document.createElement("ul");
 
-        $(roles).each(function (value) {
-            let li = document.createElement("li");
+    $(roles).each(function(value) {
+      let li = document.createElement("li");
 
-            li.innerHTML = roles[value];
+      li.innerHTML = roles[value];
 
-            ul.append(li);
-        });
+      ul.append(li);
+    });
 
-        return ul.outerHTML;
-    }
+    return ul.outerHTML;
+  }
 }
--- apps/webpack.config.js
+++ apps/webpack.config.js
@@ -4,62 +4,41 @@ const CopyWebpackPlugin = require('copy-webpack-plugin');
 Encore.setOutputPath('public/build/');
 Encore.setPublicPath('/build');
 // Encore.addEntry('app', './assets/ts/app.ts');
-Encore.addEntry('app', [
-    './assets/js/app.js',
-    './assets/scss/app.scss'
-]);
+Encore.addEntry('app', [ './assets/js/app.js', './assets/scss/app.scss' ]);
 Encore.splitEntryChunks();
 Encore.enableSingleRuntimeChunk();
 Encore.cleanupOutputBeforeBuild();
 Encore.enableBuildNotifications();
 Encore.enableSourceMaps(!Encore.isProduction());
 Encore.enableVersioning(Encore.isProduction());
-Encore.configureBabel(() => {}, {
-    'useBuiltIns': 'usage',
-    'corejs': 3
-});
-Encore.configureUrlLoader({
-    'images': {
-        'limit': 4096
-    }
-})
+Encore.configureBabel(() => {}, {'useBuiltIns' : 'usage', 'corejs' : 3});
+Encore.configureUrlLoader({'images' : {'limit' : 4096}})
 Encore.enableSassLoader();
 Encore.enableLessLoader();
 Encore.autoProvidejQuery();
 Encore.autoProvideVariables({
-    'jsPDF': 'jspdf',
-    '$': 'jquery',
-    'jQuery': 'jquery',
-    '$.formBuilder': 'formBuilder',
-    'window.jQuery': 'jquery'
+  'jsPDF' : 'jspdf',
+  '$' : 'jquery',
+  'jQuery' : 'jquery',
+  '$.formBuilder' : 'formBuilder',
+  'window.jQuery' : 'jquery'
 });
 Encore.configureBabel();
-Encore.addPlugin(new CopyWebpackPlugin([{
-        'from': 'node_modules/tinymce/skins',
-        'to': 'skins'
-    },
-    {
-        'from': 'node_modules/tinymce-i18n/langs',
-        'to': 'langs'
-    },
-    {
-        'from': 'node_modules/tinymce/plugins',
-        'to': 'plugins'
-    },
-    {
-        'from': 'node_modules/formbuilder-languages',
-        'to': 'formbuilder-lang'
-    }
+Encore.addPlugin(new CopyWebpackPlugin([
+  {'from' : 'node_modules/tinymce/skins', 'to' : 'skins'},
+  {'from' : 'node_modules/tinymce-i18n/langs', 'to' : 'langs'},
+  {'from' : 'node_modules/tinymce/plugins', 'to' : 'plugins'},
+  {'from' : 'node_modules/formbuilder-languages', 'to' : 'formbuilder-lang'}
 ]));
 // Encore.enableTypeScriptLoader();
 // Encore.enableForkedTypeScriptTypesChecking();
 if (Encore.isProduction()) {
-    Encore.enableIntegrityHashes();
+  Encore.enableIntegrityHashes();
 }
 
 let webpack = Encore.getWebpackConfig();
 
 if (Encore.isProduction()) {
-    webpack.output.jsonpFunction = 'labstag';
+  webpack.output.jsonpFunction = 'labstag';
 }
 module.exports = webpack;
