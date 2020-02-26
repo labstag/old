3fdf212503c0cbeb5881da2916d554a87d6c3073
Restyled.io <commits@restyled.io>
2020-02-26 20:14:44 +0000

    Restyled by clang-format

--- apps/assets/js/modules/xhr.js
+++ apps/assets/js/modules/xhr.js
@@ -1,33 +1,23 @@
 export class xhr {
   constructor() {}
   delete(url, data) {
-    window
-      .fetch(url, { method: "DELETE", body: JSON.stringify(data) })
-      .then(response => {
-        return response.text();
-      })
-      .then(text => {
-        return JSON.parse(text);
-      })
-      .then(json => {
-        if (json.redirect != undefined) {
-          window.location.replace(json.redirect);
-        }
-      });
+    window.fetch(url, {method : "DELETE", body : JSON.stringify(data)})
+        .then(response => { return response.text(); })
+        .then(text => { return JSON.parse(text); })
+        .then(json => {
+          if (json.redirect != undefined) {
+            window.location.replace(json.redirect);
+          }
+        });
   }
   empty(url) {
-    window
-      .fetch(url, { method: "GET" })
-      .then(response => {
-        return response.text();
-      })
-      .then(text => {
-        return JSON.parse(text);
-      })
-      .then(json => {
-        if (json.redirect != undefined) {
-          window.location.replace(json.redirect);
-        }
-      });
+    window.fetch(url, {method : "GET"})
+        .then(response => { return response.text(); })
+        .then(text => { return JSON.parse(text); })
+        .then(json => {
+          if (json.redirect != undefined) {
+            window.location.replace(json.redirect);
+          }
+        });
   }
 }
--- apps/webpack.config.js
+++ apps/webpack.config.js
@@ -4,34 +4,32 @@ const CopyWebpackPlugin = require("copy-webpack-plugin");
 Encore.setOutputPath("public/build/");
 Encore.setPublicPath("/build");
 // Encore.addEntry('app', './assets/ts/app.ts');
-Encore.addEntry("app", ["./assets/js/app.js", "./assets/scss/app.scss"]);
+Encore.addEntry("app", [ "./assets/js/app.js", "./assets/scss/app.scss" ]);
 Encore.splitEntryChunks();
 Encore.enableSingleRuntimeChunk();
 Encore.cleanupOutputBeforeBuild();
 Encore.enableBuildNotifications();
 Encore.enableSourceMaps(!Encore.isProduction());
 Encore.enableVersioning(Encore.isProduction());
-Encore.configureBabel(() => {}, { useBuiltIns: "usage", corejs: 3 });
-Encore.configureUrlLoader({ images: { limit: 4096 } });
+Encore.configureBabel(() => {}, {useBuiltIns : "usage", corejs : 3});
+Encore.configureUrlLoader({images : {limit : 4096}});
 Encore.enableSassLoader();
 Encore.enableLessLoader();
 Encore.autoProvidejQuery();
 Encore.autoProvideVariables({
-  jsPDF: "jspdf",
-  $: "jquery",
-  jQuery: "jquery",
-  "$.formBuilder": "formBuilder",
-  "window.jQuery": "jquery"
+  jsPDF : "jspdf",
+  $ : "jquery",
+  jQuery : "jquery",
+  "$.formBuilder" : "formBuilder",
+  "window.jQuery" : "jquery"
 });
 Encore.configureBabel();
-Encore.addPlugin(
-  new CopyWebpackPlugin([
-    { from: "node_modules/tinymce/skins", to: "skins" },
-    { from: "node_modules/tinymce-i18n/langs", to: "langs" },
-    { from: "node_modules/tinymce/plugins", to: "plugins" },
-    { from: "node_modules/formbuilder-languages", to: "formbuilder-lang" }
-  ])
-);
+Encore.addPlugin(new CopyWebpackPlugin([
+  {from : "node_modules/tinymce/skins", to : "skins"},
+  {from : "node_modules/tinymce-i18n/langs", to : "langs"},
+  {from : "node_modules/tinymce/plugins", to : "plugins"},
+  {from : "node_modules/formbuilder-languages", to : "formbuilder-lang"}
+]));
 // Encore.enableTypeScriptLoader();
 // Encore.enableForkedTypeScriptTypesChecking();
 if (Encore.isProduction()) {
