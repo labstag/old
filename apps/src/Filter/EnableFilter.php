eb1fc4f669a88b39a65fa26e7cbb26f6e6e89992
koromerzhin <letoullec.martial@gmail.com>
2020-03-03 16:39:05 +0100

    :ambulance: ambulance: Correction enable entity

--- apps/src/Filter/EnableFilter.php
+++ apps/src/Filter/EnableFilter.php
@@ -4,12 +4,27 @@ namespace Labstag\Filter;
 
 use Doctrine\ORM\Mapping\ClassMetadata;
 use Doctrine\ORM\Query\Filter\SQLFilter;
+use Doctrine\ORM\EntityManagerInterface;
+use \ReflectionProperty;
+use \RuntimeException;
 
 class EnableFilter extends SQLFilter
 {
+
+    /**
+     * @var EntityManagerInterface
+     */
+    protected $entityManager;
+
     public function addFilterConstraint(ClassMetadata $targetEntity, $targetTableAlias)
     {
-        unset($targetEntity);
+        $class = $targetEntity->getName();
+
+        // dump(get_class_vars($class));
+        $methods = get_class_methods($class);
+        if (!in_array('isEnable', $methods)) {
+            return '';
+        }
 
         return $targetTableAlias.'.enable IS TRUE';
     }
